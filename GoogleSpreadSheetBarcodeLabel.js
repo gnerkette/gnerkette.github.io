85b67f3145d291d21f93a77d5b49a8f11ebb40c0
Lindsey <lindsey.t@gmail.com>
2020-11-14 02:05:03 -0800

    Update GoogleSpreadSheetBarcodeLabel.js

--- GoogleSpreadSheetBarcodeLabel.js
+++ GoogleSpreadSheetBarcodeLabel.js
@@ -81,10 +81,34 @@
             {
                 var img = new Image();
                 img.crossOrigin = 'anonymous';
-           
-            	img.onload = function(){        
+                img.onload = function()
+                {
+                    try
+                    {
+                        var canvas = document.createElement('canvas');
+                        canvas.width = img.width;                     
+                        canvas.height = img.height;
+
+                        var context = canvas.getContext('2d');
+                        context.drawImage(img, 0, 0);
+
+                        var dataUrl = canvas.toDataURL('image/png');
+                        var pngBase64 = dataUrl.substr('data:image/png;base64,'.length);
+
+                        barcodeAsImageLabel.setObjectText('BARCODE', pngBase64);
+                    }
+                    catch(e)
+                    {
+                        alert(e.message || e);
+                    }
+                };
+                img.onerror = function()
+                {
+                    alert('Unable to load qr-code image');                    
+                };
+     
                 img.src = 'https://gnerkette.github.io/631060788982.jpg';
-		}
+		
             }
             catch(e)
             {
