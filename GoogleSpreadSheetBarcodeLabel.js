44570e0567314b4cd7b7559854675121d39dce1c
Lindsey <lindsey.t@gmail.com>
2020-11-14 00:55:26 -0800

    Update GoogleSpreadSheetBarcodeLabel.js

--- GoogleSpreadSheetBarcodeLabel.js
+++ GoogleSpreadSheetBarcodeLabel.js
@@ -256,16 +256,6 @@
 
                 label.print(printersSelect.value, '', labelSet);
 
-//                var records = labelSet.getRecords();
-//                for (var i = 0; i < records.length; ++i)
-//                {
-//                    label.setObjectText("SKU", records[i]["sku"]);
-//                    label.setObjectText("THEME", records[i]["theme"]);
-//                    var pngData = label.render();
-//
-//                    var labelImage = document.getElementById('img' + (i + 1));
-//                    labelImage.src = "data:image/png;base64," + pngData;
-//                }
             }
             catch (e)
             {
