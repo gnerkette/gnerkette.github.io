c3fc0d95efdb73756c53831c00cd0cb67231ac61
Lindsey <lindsey.t@gmail.com>
2020-11-15 19:56:08 -0800

    Update GoogleSpreadSheetBarcodeLabel.js

--- GoogleSpreadSheetBarcodeLabel.js
+++ GoogleSpreadSheetBarcodeLabel.js
@@ -256,7 +256,7 @@
          //       label.print(printersSelect.value, '', labelSet);
 
                 var records = labelSet.getRecords();
-		    alert(records);
+		    alert(records[0]);
                 for (var i = 0; i < records.length; ++i)
                 {
 			
